5ae4978b784dc9d48564e9f501718081a5287943
Chang Wei Sheng <cwei-she@u81z08s01.42kl.edu.my>
2023-02-07 12:09:42 +0800

    Fix 00

--- c-shell-00/ex06/git_ignore.sh
+++ c-shell-00/ex06/git_ignore.sh
@@ -1,4 +1,1 @@
-#!/bin/sh
-all_files=$(find . -type f)
-tracked_files=$(git ls-files)
-echo "$all_files" | grep -vFf <(echo "$tracked_files") | sed 's/\.\///g'
+git ls-files --ignored --others --exclude-standard
