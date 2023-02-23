51295f86ca417e82f0a08be098a38c072211f202
Willy <changws12345@gmail.com>
2023-02-23 16:05:04 +0800

    Changes were made

--- c-c-04/ex03/ft_atoi.c
+++ c-c-04/ex03/ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: cwei-she <cwei-she@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/14 22:50:34 by cwei-she          #+#    #+#             */
-/*   Updated: 2023/02/15 13:16:28 by cwei-she         ###   ########.fr       */
+/*   Updated: 2023/02/23 16:03:10 by cwei-she         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,25 +15,23 @@ int	ft_atoi(char *str)
 	int	is_negative;
 	int	num;
 
-	is_negative = 0;
+	is_negative = 1;
 	num = 0;
 	while (!(*str >= '0' && *str <= '9') && *str != '-' && *str != '+')
 	{
 		str++;
 	}
-	while (*str && (*str == '-' || *str == '+'))
+	if (*str == '-')
 	{
-		if (*str == '-')
-			is_negative = !is_negative;
+		is_negative = -1;
 		str++;
 	}
+	else if (*str == '+')
+		str++;
 	while (*str >= '0' && *str <= '9')
 	{
-		if (is_negative)
-			num = num * 10 - (*str - 48);
-		else
-			num = num * 10 + (*str - 48);
+		num = num * 10 + (*str - 48);
 		str++;
 	}
-	return (num);
+	return (is_negative * num);
 }
